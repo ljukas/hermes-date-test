74a030bd8768a3f98f1676d23c57636f598df641
Lukas Lindqvist <mail@lukaslindqvist.se>
2023-07-22 16:18:32 +0200

    added test

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
+import { useMemo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import spacetime from 'spacetime'
+
+// array of 600 items
+const arr = Array.from(Array(600).keys())
+
 
 export default function App() {
+
+  const time = useMemo(() => {
+    const now = performance.now()
+    arr.forEach((item, index) => {
+      spacetime('2015-05-25').add(index, 'days')
+    })
+    const end = performance.now()
+
+    console.log('spacetime ms', end - now)
+    return end - now
+  }, [])
+
+  const time2 = useMemo(() => {
+    const now = performance.now()
+    arr.forEach((item, index) => {
+      const initial = new Date('2015-05-25').getTime()
+      new Date(initial+ index * 86400000)
+    })
+    const end = performance.now()
+
+    console.log('date ms', end - now)
+    return end - now
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Spacetime took {time} ms</Text>
+      <Text>Date took {time2} ms</Text>
       <StatusBar style="auto" />
     </View>
   );
